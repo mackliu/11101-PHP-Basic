ca135f08de22d9fa6836b353ea58408131f3c725
mackliu <macklun@gmail.com>
2022-04-22 13:48:23 +0800

    完成字串取代成星星的練習

---
+++ string/pra01.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>字串取代</h1>
+<article>
+<h3>字串取代</h3>
+<div>將”aaddw1123”改成”*********”</div>
+
+</article>
+<?php
+$password="aadd";
+//str_replace() 
+//$password=str_replace("a","*",$password);
+$strlen=mb_strlen($password);
+$password=str_repeat("*",$strlen);
+echo $password;
+?>
+</body>
+</html>
