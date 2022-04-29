2f3bc9e1ec863e76a781daa9ba878863a3984640
mackliu <macklun@gmail.com>
2022-04-29 16:24:05 +0800

    改用flex box來做月曆的呈現

--- date/pra06.php
+++ date/pra06.php
@@ -6,14 +6,30 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>線上月曆</title>
     <style>
-        table{
-            border-collapse: collapse;
+        .table{
+            width:560px;
+            height:560px;
+            /* border:1px solid green; */
+            display:flex;
+            flex-wrap:wrap;
+            align-content: baseline;
+            margin-left:1px;
+            margin-top:1px;
         }
 
-        table td{
-            padding:5px;
-            text-align: center;
-            border:1px solid #aaa;
+        .table div{
+            display:inline-block;
+            width:80px;
+            height:80px;
+            border:1px solid #999;
+            box-sizing: border-box;
+            margin-left:-1px;
+            margin-top:-1px;
+        }
+        .table div.header{
+            background:black;
+            color:white;
+            height: 32px;;
         }
         .weekend{
             background:pink;
@@ -31,18 +47,6 @@
 <?php
 $month=2;
 
-?>
-<table>
-    <tr>
-        <td>日</td>
-        <td>一</td>
-        <td>二</td>
-        <td>三</td>
-        <td>四</td>
-        <td>五</td>
-        <td>六</td>
-    </tr>
-<?php
 
 $firstDay=date("Y-").$month."-1";
 $firstWeekday=date("w",strtotime($firstDay));
@@ -65,101 +69,31 @@ for($i=0;$i<(6-$lastWeekday);$i++){
     $dateHouse[]="";
 }
 
-echo "<pre>";
-print_r($dateHouse);
-echo "</pre>";
+?>
 
-foreach($dateHouse as $key=>$day){
-
-    if($key%7==0){
-        echo "<tr>";
-    }
-
+<div class="table">
+<div class='header'>日</div>
+<div class='header'>一</div>
+<div class='header'>二</div>
+<div class='header'>三</div>
+<div class='header'>四</div>
+<div class='header'>五</div>
+<div class='header'>六</div>
+<?php
+foreach($dateHouse as $k => $day){
+    $hol=($k%7==0 || $k%7==6)?'weekend':"";
+    
     if(!empty($day)){
-        $dayFormat=date("d",strtotime($day));
+        $dayFormat=date("j",strtotime($day));
+        echo "<div class='{$hol}'>{$dayFormat}</div>";
     }else{
-        $dayFormat="";
+        echo "<div class='{$hol}'></div>";
+
     }
-
-    //$dayFormat=(!empty($day))?date("d",strtotime($day)):"";
-
-    echo "<td>{$dayFormat}</td>";
-
-    if($key%7==6){
-        echo "</tr>";
-    }
-    
 }
 
-
 ?>
-</table>
-<hr>
-<?php
-echo "月份".$month;
-echo "<br>";
-echo "第一天是".$firstDay;
-echo "<br>";
-echo "第一天是星期".$firstWeekday;
-echo "<br>";
-echo "最後一天是".$lastDay;
-echo "<br>";
-echo "當月天數共".$monthDays;
-echo "<br>";
-?>
-
-
-<table>
-    <tr>
-        <td>日</td>
-        <td>一</td>
-        <td>二</td>
-        <td>三</td>
-        <td>四</td>
-        <td>五</td>
-        <td>六</td>
-    </tr>
-<?php
-
-for($i=0;$i<6;$i++){
-    echo "<tr>";
-    
-    for($j=0;$j<7;$j++){
-        $d=$i*7+($j+1)-$firstWeekday-1;
-        
-        if($d>=0 && $d<$monthDays){
-            $fs=strtotime($firstDay);
-            $shiftd=strtotime("+$d days",$fs);
-            $date=date("d",$shiftd);
-            $w=date("w",$shiftd);
-            $chktoday="";
-            if(date("Y-m-d",$shiftd)==$today){
-                $chktoday='today';
-            }
-            //$date=date("Y-m-d",strtotime("+$d days",strtotime($firstDay)));
-            if($w==0 || $w==6){
-                echo "<td class='weekend $chktoday' >";
-            }else{
-                echo "<td class='workday $chktoday'>";
-            }
-            echo $date;
-            echo "</td>";
-        }else{
-            echo "<td></td>";
-        }
-            
-
-    }
-
-    echo "</tr>";
-}
-
-
-
-?>
-
-
-</table>
+</div>
 
 
 
